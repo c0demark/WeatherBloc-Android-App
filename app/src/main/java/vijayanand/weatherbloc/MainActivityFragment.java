1c939413f35b91e1f1f2194f076b2a7779cf23ce
vijayanand-pg <vijay_006@hotmail.com>
2016-09-21 15:52:00 +0530

    Added OpenWeatherMap API

--- app/src/main/java/vijayanand/weatherbloc/MainActivityFragment.java
+++ app/src/main/java/vijayanand/weatherbloc/MainActivityFragment.java
@@ -2,12 +2,19 @@ package vijayanand.weatherbloc;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -19,7 +26,7 @@ public class MainActivityFragment extends Fragment {
 
     private ListView listView;
     ArrayAdapter<String> mForecastAdapter;
-    ArrayList<String> messages;
+    ArrayList<String> weekForecast;
 
     public MainActivityFragment() {
     }
@@ -55,6 +62,51 @@ public class MainActivityFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         ListView listview = (ListView) rootView.findViewById(R.id.list_view_forecast);
         listview.setAdapter(mForecastAdapter);
+
+        HttpURLConnection urlconnection = null;
+        BufferedReader reader = null;
+        String forecastJsonStr = null;
+
+        try{
+            URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=600073mode=json&units=metric&cnt=7");
+            urlconnection = (HttpURLConnection) url.openConnection();
+            urlconnection.setRequestMethod("GET");
+            urlconnection.connect();
+
+            InputStream inputStream = urlconnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if(inputStream == null){
+                forecastJsonStr = null;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while((line = reader.readLine()) != null){
+                buffer.append(line + '\n');
+            }
+            if(buffer.length() == 0){
+                forecastJsonStr = null;
+            }
+            forecastJsonStr = buffer.toString();
+        }
+        catch(IOException e){
+            Log.e("MainActivityFragment", "Error", e);
+            forecastJsonStr = null;
+        }
+        finally {
+            if (urlconnection != null){
+                urlconnection.disconnect();
+            }
+            if(reader != null){
+                try{
+                    reader.close();
+                }
+                catch(final IOException e){
+                    Log.e("MainActivityFragment", "Error closing stream",e);
+                }
+            }
+        }
+
+
         return rootView;
     }
 }
