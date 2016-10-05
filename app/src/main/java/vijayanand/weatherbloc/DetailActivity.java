d9c609bcbc97790de7baa25babc01ebbe5bf6f73
vijayanand-pg <vijay_006@hotmail.com>
2016-10-05 11:15:25 +0530

    Added SQLiteDB Location and weather entry

--- app/src/main/java/vijayanand/weatherbloc/DetailActivity.java
+++ app/src/main/java/vijayanand/weatherbloc/DetailActivity.java
@@ -5,7 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.MenuItemCompat;
-+import android.support.v7.widget.ShareActionProvider;
+import android.support.v7.widget.ShareActionProvider;
 import android.util.Log;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
