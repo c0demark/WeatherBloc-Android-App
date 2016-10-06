3767e7bc4d6a95603840a5dfa9ebc56ba40372d3
vijayanand-pg <vijay_006@hotmail.com>
2016-10-06 17:44:18 +0530

    Content contract Test weather

---
+++ app/src/androidTest/java/vijayanand.weatherbloc/data/TestProvider.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Home on 06-10-2016.
+ */
+public class TestProvider {
+}
---
+++ app/src/main/java/vijayanand/weatherbloc/FetchWeatherTask.java
@@ -0,0 +1,7 @@
+package vijayanand.weatherbloc;
+
+/**
+ * Created by Home on 06-10-2016.
+ */
+public class FetchWeatherTask {
+}
